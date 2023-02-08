5691c8a143ecb5ef8dd59e5608c3a69bde76c865
Xun Yi <xunyi.work@gmail.com>
2023-02-08 19:32:38 +0800

    Attempt 1 at creating form

---
+++ src/components/FormComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div v-if="type=='text'">
+        <input type="text" placeholder="Key in title"/> 
+        <span>Input field here</span>
+    </div>
+    
+</template>
+
+<script>
+    export default {
+        props: ["type"],
+        setup(props) {
+            console.log(props.title);
+            return {props, exportMe};
+        },
+    };
+    function exportMe(){
+        return "Fuck yuo";
+    }
+</script>
---
+++ src/views/FormBuilder.vue
@@ -0,0 +1,72 @@
+<template>
+    <Navbar/>
+    <div class="row mx-5">
+        <div class="col-10">
+            <div v-for="item in formItems" ref="formComponents">
+                <FormComponent :type="item" />
+            </div>
+        </div>
+        
+        <div class="col-2">
+            <div class="row">
+                <button @click="addText" class="btn btn-main-blue">Add Text</button>
+                <button @click="addTextInput" class="btn btn-main-blue">Add Text Input</button>                
+            </div>
+            <div class="row">
+                <button @click="test" class="btn btn-turqouise" >Export Form</button>                
+            </div>
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+import Navbar from "../components/Navbar.vue";
+import UserService from "../services/userService";
+import FormComponent from "../components/FormComponent.vue";
+import { ref } from "vue";
+
+export default {
+    components: {
+        Navbar,
+        FormComponent,
+    },
+    setup() {
+        var content = ref("");
+        UserService.getUserBoard().then(
+        (response) => {
+            content.value = response.data;
+        },
+        (error) => {
+            content.value =
+            (error.response && error.response.data) ||
+            error.message ||
+            error.toString();
+        }
+        );
+       
+        const formItems = ref(["item1"]);
+        const formComponents = ref([null]);
+        return { content, formItems, addText, addTextInput, test, formComponents};
+    },
+};
+
+
+
+function addText(){
+    this.formItems.push("Sample Headers");
+};
+function addTextInput(){
+    this.formItems.push("text");
+};
+
+function test(){
+    console.log(this.formComponents);
+    console.log(typeof this.formComponents);
+    for (const components in this.formComponents){
+        console.log(this.formComponents[components]);
+    }
+    
+}
+</script>
