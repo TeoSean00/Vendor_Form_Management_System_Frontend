b6581e0bc52fb80e577b0c8a3697a9b72d91c5d6
JosephJL <HeeJLJoseph@gmail.com>
2023-02-09 09:30:38 +0800

    Updated Formbuilder and Formcomponent

--- src/components/FormComponent.vue
+++ src/components/FormComponent.vue
@@ -1,34 +1,35 @@
 <template>
-    <div v-if="type=='text'">
-        <input type="text" placeholder="Key in title" v-model="title" @input="updateQuestion"/> 
-        <span>{{idx}} Input field here</span>
-    </div>
-    
+  <div v-if="type == 'text'">
+    <input
+      type="text"
+      placeholder="Key in title"
+      v-model="title"
+      @input="updateQuestion"
+    />
+    <span>{{ idx }} Input field here</span>
+  </div>
 </template>
 
 <script>
 import { ref } from "vue";
 
 export default {
-    props: ["type", "idx"],
-    emits: ["updateQuestion"],
-    setup(props, context) {
-        const title = ref();
-        return {props, context, updateQuestion, title};
-    },
-};
+  props: ["type", "idx"],
+  emits: ["updateQuestion"],
+  setup(props, context) {
+    const title = ref();
 
-function updateQuestion(){
-    const componentInfo = {
-        "type" : this.props.type,
-        "order" : this.props.idx,
-        "text" : this.title,
+    function updateQuestion() {
+      const componentInfo = {
+        type: props.type,
+        order: props.idx,
+        text: title.value,
+      };
+      context.emit("updateQuestion", componentInfo);
+      // console.log(componentInfo);
     }
-    this.context.emit('updateQuestion', componentInfo);
-    // console.log(componentInfo);
-}
 
-
+    return { props, context, updateQuestion, title };
+  },
+};
 </script>
-
-
--- src/views/FormBuilder.vue
+++ src/views/FormBuilder.vue
@@ -1,24 +1,24 @@
 <template>
-    <Navbar/>
-    <div class="row mx-5">
-        <div class="col-10">
-            <div v-for="(item,index) in formItems"  >
-                <FormComponent :type="item" :idx='index' @updateQuestion="update"/>
-            </div>
-        </div>
-        
-        <div class="col-2">
-            <div class="row">
-                <button @click="addText" class="btn btn-main-blue">Add Text</button>
-                <button @click="addTextInput" class="btn btn-main-blue">Add Text Input</button>                
-            </div>
-            <div class="row">
-                <button @click="test" class="btn btn-turqouise" >Export Form</button>                
-            </div>
-        </div>
-
+  <Navbar />
+  <div class="row mx-5">
+    <div class="col-10">
+      <div v-for="(item, index) in formItems" :key="index">
+        <FormComponent :type="item" :idx="index" @updateQuestion="update" />
+      </div>
     </div>
 
+    <div class="col-2">
+      <div class="row">
+        <button @click="addText" class="btn btn-main-blue">Add Text</button>
+        <button @click="addTextInput" class="btn btn-main-blue">
+          Add Text Input
+        </button>
+      </div>
+      <div class="row">
+        <button @click="test" class="btn btn-turqouise">Export Form</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -28,44 +28,51 @@ import FormComponent from "../components/FormComponent.vue";
 import { ref } from "vue";
 
 export default {
-    components: {
-        Navbar,
-        FormComponent,
-    },
-    setup() {
-        var content = ref("");
-        UserService.getUserBoard().then(
-        (response) => {
-            content.value = response.data;
-        },
-        (error) => {
-            content.value =
-            (error.response && error.response.data) ||
-            error.message ||
-            error.toString();
-        }
-        );
-       
-        const formItems = ref([]);
-        const formOutput = ref([]);
-        return { content, formItems, formOutput, addText, addTextInput, update};
-    },
+  components: {
+    Navbar,
+    FormComponent,
+  },
+  setup() {
+    var content = ref("");
+
+    UserService.getUserBoard().then(
+      (response) => {
+        content.value = response.data;
+      },
+      (error) => {
+        content.value =
+          (error.response && error.response.data) ||
+          error.message ||
+          error.toString();
+      }
+    );
+
+    var formItems = ref([]);
+    var formOutput = ref({});
+
+    addText();
+    addTextInput();
+
+    function addText() {
+      formItems.value.push("Sample Headers");
+    }
+    function addTextInput() {
+      formItems.value.push("text");
+    }
+
+    function update(data) {
+      console.log(data);
+      formOutput.value[data.order] = data;
+      //   for (item in formItems.value) {
+      //     console.log(data);
+      //     console.log(formItems.value);
+      //     console.log(formOutput.value);
+      //   }
+      // this.formOutput.push(data);
+      console.log(formOutput.value);
+    }
+
+    return { content, formItems, addText, addTextInput, update };
+  },
 };
-
-
-
-function addText(){
-    this.formItems.push("Sample Headers");
-};
-function addTextInput(){
-    this.formItems.push("text");
-};
-
-function update(data){
-    console.log(data);
-    console.log(this.formItems);
-    console.log(this.formOutput);
-    // this.formOutput.push(data);
-
-}
 </script>
