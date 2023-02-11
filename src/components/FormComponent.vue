c05617e6840420ce70088f5c9623d60fd6067b96
Xun Yi <xunyi.work@gmail.com>
2023-02-11 17:15:52 +0800

    Working on delete functionality

--- src/components/FormComponent.vue
+++ src/components/FormComponent.vue
@@ -1,35 +1,42 @@
 <template>
   <div v-if="type == 'text'">
-    <input
-      type="text"
-      placeholder="Key in title"
-      v-model="title"
-      @input="updateQuestion"
+    <TextInput
+      :idx="idx"
+      @updateText="updateQuestion"
+      @removeQuestion="remove"
     />
-    <span>{{ idx }} Input field here</span>
   </div>
 </template>
 
 <script>
 import { ref } from "vue";
 
+import TextInput from "./formComponents/TextInput.vue";
+
 export default {
   props: ["type", "idx"],
-  emits: ["updateQuestion"],
+  emits: ["updateQuestion", "remove"],
+  components: {
+    TextInput,
+  },
   setup(props, context) {
     const title = ref();
 
-    function updateQuestion() {
-      const componentInfo = {
-        type: props.type,
-        order: props.idx,
-        text: title.value,
-      };
+    function updateQuestion(componentInfo) {
+      // const componentInfo = {
+      //   type: props.type,
+      //   order: props.idx,
+      //   text: title.value,
+      // };
       context.emit("updateQuestion", componentInfo);
-      // console.log(componentInfo);
     }
 
-    return { props, context, updateQuestion, title };
+    function remove(questionKey) {
+      //Passes from FormComponent > ForBuilder
+      context.emit("remove", questionKey);
+    }
+
+    return { props, context, title, updateQuestion, remove };
   },
 };
 </script>
