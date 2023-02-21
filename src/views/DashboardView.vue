50c2850341f236f1b02267c162c5b45ff2a731db
TeoSean00 <teosean@outlook.com>
2023-02-21 14:38:46 +0800

    dashboard file restructuring

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-import { Dashboard } from "../components/Dashboard.vue";
+import Dashboard  from "../components/dashboard/Dashboard.vue";
 
 export default {
   setup() {
@@ -15,31 +15,11 @@ export default {
       2:2},
       ['hi'],
     )
-
-
-
     // method syntax
     const getValue = () => {
       pass
     }
-
-
-
     return { getValue, data }
-    // var content = ref("");
-
-    // UserService.getPublicContent().then(
-    //   (response) => {
-    //     content.value = response.data;
-    //   },
-    //   (error) => {
-    //     content.value =
-    //       (error.response && error.response.data) ||
-    //       error.message ||
-    //       error.toString();
-    //   }
-    // );
-    // return { content };
   },
 };
 </script>
