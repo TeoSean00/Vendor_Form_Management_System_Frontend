47801a6f6db32870cd9b4d6a9877dd6890998b4d
TeoSean00 <teosean@outlook.com>
2023-03-05 20:20:20 +0800

    skeletons for all dashboards done, with dummy data for now

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,15 +1,23 @@
 <template>
   <AdminHomeDashboard />
+  <AdminVendorPageDashboard />
+  <VendorHomeDashboard />
 </template>
 
 <script>
 import AdminHomeDashboard from "../components/dashboard/AdminHomePage/AdminHomeDashboard.vue";
+import AdminVendorPageDashboard from "../components/dashboard/AdminVendorPage/AdminVendorPageDashboard.vue";
+import VendorHomeDashboard from "../components/dashboard/VendorHomePage/VendorHomeDashboard.vue";
 import { ref } from "vue";
 
 export default {
   components: {
     AdminHomeDashboard,
+    AdminVendorPageDashboard,
+    VendorHomeDashboard
   },
-  setup() {},
+  setup() {
+
+  },
 };
 </script>
