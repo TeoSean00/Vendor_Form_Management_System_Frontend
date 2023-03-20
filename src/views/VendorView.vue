109a2f5c881c63c27bef54ab8f8927867ae5caa4
JosephJL <HeeJLJoseph@gmail.com>
2023-03-20 12:18:16 +0800

    Update VendorView.vue

--- src/views/VendorView.vue
+++ src/views/VendorView.vue
@@ -1,10 +1,24 @@
-<template><Navbar />VENDOR PAGE HERE</template>
+<template><Navbar />VENDOR PAGE HERE{{ auth }}{{ vendorInfo }}</template>
 
 <script>
 import Navbar from "../components/navbar/Navbar.vue";
+import { useAuthStore } from "../stores/authStore";
+import VendorService from "../services/vendor/vendorService";
+import { ref } from "vue";
 
 export default {
   components: { Navbar },
-  setup() {},
+  setup() {
+    var auth = useAuthStore();
+    var currentUser = auth.user;
+
+    var vendorInfo = ref(null);
+    var getVendorInfo = async () => {
+      vendorInfo.value = await VendorService.getVendor(currentUser.vendorId);
+    };
+    getVendorInfo();
+
+    return { auth, vendorInfo };
+  },
 };
 </script>
