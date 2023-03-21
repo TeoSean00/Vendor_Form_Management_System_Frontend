9bc2826f37fb83a40592e496f755ca4a779d2652
JosephJL <HeeJLJoseph@gmail.com>
2023-03-21 20:22:43 +0800

    updated CreaterUser, ManageVendorView
    Error handling and toast notifications

--- src/components/navbar/NavbarJP.vue
+++ src/components/navbar/NavbarJP.vue
@@ -1,121 +1,142 @@
 <template>
-    <div class="container-fluid bluebg d-flex justify-content-between m-0" style="width: 100%;">
-      <div>
-        <router-link to="/vendorView" class="nav-link text-light" v-if="isUser">
-          <img src="../../assets/Logo.png" alt="" style="width: 80px; height: 80px;" class="mt-3 mx-5">
-        </router-link>
-        <router-link to="/" class="nav-link text-light" v-else>
-          <img src="../../assets/Logo.png" alt="" style="width: 80px; height: 80px;" class="mt-3 mx-5">
-        </router-link>
-      </div>
-      <div>
-        <nav class="navbar navbar-expand navbar-light mt-5 px-0">
-    
-          <a href="" class="navbar-brand" @click.prevent></a>
-          
-          <div class="navbar-nav mr-auto mx-4">
-  
-            <li class="nav-item " v-if="isUser">
-              <router-link to="/vendorView" class="nav-link text-light">
-                <font-awesome-icon class="text-light" style="width: 25px; height: 25px; " icon="home"/>
-              </router-link>
-            </li>
-            <li class="nav-item " v-else>
-              <router-link to="/" class="nav-link text-light">
-                <font-awesome-icon class="text-light" style="width: 25px; height: 25px; " icon="home"/>
-              </router-link>
-            </li>
-            <!-- <li class="nav-item">
+  <div
+    class="container-fluid bluebg d-flex justify-content-between m-0"
+    style="width: 100%"
+  >
+    <div>
+      <router-link to="/vendorView" class="nav-link text-light" v-if="isUser">
+        <img
+          src="../../assets/Logo.png"
+          alt=""
+          style="width: 80px; height: 80px"
+          class="mt-3 mx-5"
+        />
+      </router-link>
+      <router-link to="/" class="nav-link text-light" v-else>
+        <img
+          src="../../assets/Logo.png"
+          alt=""
+          style="width: 80px; height: 80px"
+          class="mt-3 mx-5"
+        />
+      </router-link>
+    </div>
+    <div>
+      <nav class="navbar navbar-expand navbar-light mt-5 px-0">
+        <a href="" class="navbar-brand" @click.prevent></a>
+
+        <div class="navbar-nav mr-auto mx-4">
+          <li class="nav-item" v-if="isUser">
+            <router-link to="/vendorView" class="nav-link text-light">
+              <font-awesome-icon
+                class="text-light"
+                style="width: 25px; height: 25px"
+                icon="home"
+              />
+            </router-link>
+          </li>
+          <li class="nav-item" v-else>
+            <router-link to="/" class="nav-link text-light">
+              <font-awesome-icon
+                class="text-light"
+                style="width: 25px; height: 25px"
+                icon="home"
+              />
+            </router-link>
+          </li>
+          <!-- <li class="nav-item">
               <router-link to="#" class="nav-link text-light">
                 <font-awesome-icon class="text-light" style="width: 25px; height: 25px; " icon="bell" />
               </router-link>
             </li> -->
-            <li class="nav-item ">
-              <router-link to="/formbuilder" class="nav-link text-light">
-                <font-awesome-icon class="text-light" style="width: 25px; height: 25px; " icon="clipboard" />
-              </router-link>
-            </li>
-            <!-- <li class="nav-item">
+          <li class="nav-item">
+            <router-link to="/formbuilder" class="nav-link text-light">
+              <font-awesome-icon
+                class="text-light"
+                style="width: 25px; height: 25px"
+                icon="clipboard"
+              />
+            </router-link>
+          </li>
+          <!-- <li class="nav-item">
               <router-link to="/profile" class="nav-link text-light">
                 <font-awesome-icon class="text-light" style="width: 25px; height: 25px; " icon="user" />
               </router-link>
             </li> -->
-            <!-- <li v-if="showAdminBoard" class="nav-item">
+          <!-- <li v-if="showAdminBoard" class="nav-item">
               <router-link to="/admin" class="nav-link">Admin Board</router-link>
             </li>
             <li v-if="showModeratorBoard" class="nav-item">
               <router-link to="/mod" class="nav-link">Moderator Board</router-link>
             </li> -->
-            <!-- <li class="nav-item">
+          <!-- <li class="nav-item">
               <router-link v-if="currentUser" to="/user" class="nav-link"
                 >User</router-link
               >
             </li> -->
-          </div>
-    
-          <div v-if="!currentUser" class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <router-link to="/register" class="nav-link text-light">
-                <!-- <font-awesome-icon icon="user-plus" /> -->
-                &nbspSign Up
-              </router-link>
-            </li>
-            <li class="nav-item">
-              <router-link to="/login" class="nav-link text-light">
-                <!-- <font-awesome-icon icon="sign-in-alt" /> -->
-                &nbspLogin
-              </router-link>
-            </li>
-          </div>
-    
-          <div v-if="currentUser" class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <router-link to="/profile" class="nav-link text-light">
-                {{ currentUser.username }}
-              </router-link>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link text-light" href @click.prevent="logOut">
-                <!-- <font-awesome-icon icon="sign-out-alt" /> -->
-                Log Out
-              </a>
-            </li>
-          </div>
-        </nav>
-  
-      </div>
+        </div>
+
+        <div v-if="!currentUser" class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <router-link to="/register" class="nav-link text-light">
+              <!-- <font-awesome-icon icon="user-plus" /> -->
+              &nbsp; Sign Up
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/login" class="nav-link text-light">
+              <!-- <font-awesome-icon icon="sign-in-alt" /> -->
+              &nbsp; Login
+            </router-link>
+          </li>
+        </div>
+
+        <div v-if="currentUser" class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <router-link to="/profile" class="nav-link text-light">
+              {{ currentUser.username }}
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-light" href @click.prevent="logOut">
+              <!-- <font-awesome-icon icon="sign-out-alt" /> -->
+              Log Out
+            </a>
+          </li>
+        </div>
+      </nav>
     </div>
-  </template>
-  <script>
-  import { ref } from "vue";
-  import { useAuthStore } from "../../stores/authStore";
-  export default {
-    setup() {
-      var auth = useAuthStore();
-      var currentUser = ref(auth.user);
-      var isUser = ref(currentUser.value.roles.includes("ROLE_USER"))
-      const showAdminBoard = () => {
-        console.log(currentUser);
-        console.log(currentUser.value);
-        if (currentUser.value && currentUser.value.roles) {
-          currentUser.value.roles.includes("ROLE_ADMIN");
-        }
-        return false;
-      };
-      const showModeratorBoard = () => {
-        if (currentUser.value && currentUser.value.roles) {
-          return currentUser.value.roles.includes("ROLE_MODERATOR");
-        }
-        return false;
-      };
-  
-      const logOut = async () => {
-        console.log("logout called");
-        console.log(auth);
-        auth.logout();
-      };
-      return { currentUser, showAdminBoard, showModeratorBoard, logOut, isUser };
-    },
-  };
-  </script>
-  
+  </div>
+</template>
+<script>
+import { ref } from "vue";
+import { useAuthStore } from "../../stores/authStore";
+export default {
+  setup() {
+    var auth = useAuthStore();
+    var currentUser = ref(auth.user);
+    var isUser = ref(currentUser.value.roles.includes("ROLE_USER"));
+    const showAdminBoard = () => {
+      console.log(currentUser);
+      console.log(currentUser.value);
+      if (currentUser.value && currentUser.value.roles) {
+        currentUser.value.roles.includes("ROLE_ADMIN");
+      }
+      return false;
+    };
+    const showModeratorBoard = () => {
+      if (currentUser.value && currentUser.value.roles) {
+        return currentUser.value.roles.includes("ROLE_MODERATOR");
+      }
+      return false;
+    };
+
+    const logOut = async () => {
+      console.log("logout called");
+      console.log(auth);
+      auth.logout();
+    };
+    return { currentUser, showAdminBoard, showModeratorBoard, logOut, isUser };
+  },
+};
+</script>
--- src/views/ManageVendorView.vue
+++ src/views/ManageVendorView.vue
@@ -125,6 +125,7 @@
                 class="form-control"
                 v-model="newVendorName"
                 placeholder="Vendor name here"
+                required
               />
             </div>
             <div class="mb-3">
@@ -134,6 +135,7 @@
                 class="form-control"
                 v-model="newVendorCountry"
                 placeholder="Vendor country here"
+                required
               />
             </div>
             <div class="mb-3">
@@ -145,7 +147,15 @@
                 id="exampleFormControlTextarea1"
                 rows="3"
                 v-model="newVendorDetails"
+                required
               ></textarea>
+            </div>
+            <div v-if="errors.length">
+              <template v-for="(error, index) in errors" :key="index"
+                ><div class="alert alert-warning" role="alert">
+                  You selected {{ error }}
+                </div></template
+              >
             </div>
           </div>
           <div class="modal-footer">
@@ -159,7 +169,7 @@
             <button
               type="button"
               class="btn btn-primary"
-              data-bs-dismiss="modal"
+              :data-bs-dismiss="errors.length != 0 ? 'modal' : ''"
               @click="toggleNewVendor(newVendorName)"
             >
               Create Vendor
@@ -226,26 +236,48 @@ export default {
 
     const router = useRouter();
 
+    var errors = ref([]);
     var newVendorName = ref("");
     var newVendorDetails = ref("");
     var newVendorCountry = ref("");
 
+    var checkForm = () => {
+      console.log("check form is called!");
+      let check = true;
+      if (newVendorName.value == "") {
+        errors.value.push("Vendor name cannot be left blank");
+        check = false;
+      }
+      if (newVendorCountry.value == "") {
+        errors.value.push("Vendor country cannot be left blank");
+        check = false;
+      }
+
+      if (newVendorDetails.value == "") {
+        errors.value.push("Vendor details cannot be left blank");
+        check = false;
+      }
+      return check;
+    };
+
     const toggleNewVendor = (newVendorName) => {
-      console.log("toggle create new vendor", newVendorName);
-      console.log("SUMITTED INFO");
-      console.log(newVendorCountry.value);
-      console.log(newVendorName.value);
-      console.log(newVendorDetails.value);
-      var newVendor = {
-        name: newVendorName,
-        details: newVendorDetails.value,
-        country: newVendorCountry.value,
-        forms: [],
-      };
-      vendorStore.addVendor(newVendor);
-      newVendorName.value = "";
-      newVendorCountry = "";
-      newVendorDetails = "";
+      // console.log("toggle create new vendor", newVendorName);
+      // console.log("SUMITTED INFO");
+      // console.log(newVendorCountry.value);
+      // console.log(newVendorName.value);
+      // console.log(newVendorDetails.value);
+      if (checkForm()) {
+        var newVendor = {
+          name: newVendorName,
+          details: newVendorDetails.value,
+          country: newVendorCountry.value,
+          forms: [],
+        };
+        vendorStore.addVendor(newVendor);
+        newVendorName.value = "";
+        newVendorCountry = "";
+        newVendorDetails = "";
+      }
     };
 
     const toggleEditVendor = (vendor) => {
@@ -277,6 +309,7 @@ export default {
     console.log(filteredNames);
 
     return {
+      errors,
       newVendorCountry,
       searchName,
       filteredNames,
