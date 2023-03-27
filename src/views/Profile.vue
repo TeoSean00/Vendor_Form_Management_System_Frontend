5a4a972d3c60adecf28434f676e27e6ae4782b4b
Xun Yi <xunyi.work@gmail.com>
2023-03-27 17:42:00 +0800

    Modified profile page.

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,7 +1,32 @@
 <template>
   <div class="container-flex">
     <Navbar />
-    <header class="jumbotron">
+    <div class="col-4 offset-4 mt-5 p-5 rounded shadow">
+      <h1> Profile </h1>
+      <div class="form-group row">
+        <label for="userName" class="col-sm-2 col-form-label text-dark-grey">Name</label>
+        <div class="col-sm-10">
+          <input type="text" readonly class="form-control-plaintext" id="userName" :value="currentUser.username">
+        </div>
+      </div>
+      <div class="form-group row">
+        <label for="email" class="col-sm-2 col-form-label text-dark-grey">Email</label>
+        <div class="col-sm-10">
+          <input type="text" readonly class="form-control-plaintext" id="email" :value="currentUser.email">
+        </div>
+      </div>
+      <div class="form-group row">
+        <label for="roles" class="col-sm-2 col-form-label text-dark-grey">Roles</label>
+        <div class="col-sm-10">
+          <ul>
+            <li v-for="(role, index) in currentUser.roles" :key="index">
+              {{ role }}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <!-- <header class="jumbotron">
       <h3>
         <strong>{{ currentUser.username }}</strong> Profile
       </h3>
@@ -24,14 +49,16 @@
       <li v-for="(role, index) in currentUser.roles" :key="index">
         {{ role }}
       </li>
-    </ul>
+    </ul> -->
+
+
   </div>
 </template>
 
 <script>
 import { useAuthStore } from "../stores/authStore";
 import { useRouter } from "vue-router";
-import Navbar from "../components/navbar/Navbar.vue";
+import Navbar from "../components/navbar/NavbarJP.vue";
 export default {
   components: { Navbar },
   setup() {
