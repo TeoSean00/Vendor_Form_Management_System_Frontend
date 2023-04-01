3dd124bd2f919252a1420aa9aa2aea080fd66815
TeoSean00 <teosean@outlook.com>
2023-04-01 21:57:54 +0800

    deadLineChart completed

--- src/components/dashboard/AdminVendorPage/AdminVendorPageDashboard.vue
+++ src/components/dashboard/AdminVendorPage/AdminVendorPageDashboard.vue
@@ -1,17 +1,14 @@
 <template>
   <div class="container">
-    <h2 class="mt-5 mb-4">Admin's Vendor Page Dashboards</h2>
     <div class="row">
       <!-- 1st chart -->
       <div class="col">
         <FormStatusBarChart vendorID="6409bf14f5a8a3592938ed73" />
       </div>
-
       <!-- 2nd chart -->
       <div class="col">
         <UpdatesTodayChart />
       </div>
-
       <!-- 3rd chart -->
       <div class="col">
         <DeadlinesChart />
