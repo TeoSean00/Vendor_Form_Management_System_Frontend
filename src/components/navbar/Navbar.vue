3e128ebc1e7d930d5028432a3603272b088eddb7
Tyler Lian <65641550+anahopbob@users.noreply.github.com>
2023-04-02 14:04:30 +0800

    navbar

--- src/components/navbar/Navbar.vue
+++ src/components/navbar/Navbar.vue
@@ -14,20 +14,6 @@
               <font-awesome-icon icon="home" />Home
             </router-link>
           </li>
-          <!-- <li v-if="showAdminBoard" class="nav-item">
-            <router-link to="/admin" class="nav-link">Admin Board</router-link>
-          </li>
-          <li v-if="showModeratorBoard" class="nav-item">
-            <router-link to="/mod" class="nav-link"
-              >Moderator Board</router-link
-            >
-          </li>
-          <li class="nav-item">
-            <router-link v-if="currentUser" to="/user" class="nav-link"
-              >User</router-link
-            >
-          </li> -->
-
           <div v-if="!currentUser" class="navbar-nav m-auto">
             <li class="nav-item">
               <router-link to="/login" class="nav-link">
